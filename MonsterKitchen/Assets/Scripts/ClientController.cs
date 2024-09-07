56a634673b5c41e12e99a02f4ffc60dd8d48e10f
Damian Lobos <damianlobos24@gmail.com>
2024-09-07 10:55:39 -0300

    Orders

--- MonsterKitchen/Assets/Scripts/ClientController.cs
+++ MonsterKitchen/Assets/Scripts/ClientController.cs
@@ -11,11 +11,18 @@ public class ClientController : MonoBehaviour
     [SerializeField] private bool waitForATable;
     [SerializeField] private TMP_Text text;
     [SerializeField] private TMP_Text temporizador;
+    private bool waitForOrder;
     private Transform chairPosition;
     private bool canInteract = true;
+    private bool collisioning = false;
 
     private int timeToWaitTable;
+    private int timeToWaitToOrder;
+    private int waitToOrder = 0;
     private float speed = 3f;
+    public bool WaitForOrder { get => waitForOrder; set => waitForOrder = value; }
+    public TMP_Text Text { get => text; set => text = value; }
+    public int WaitToOrder { get => waitToOrder; set => waitToOrder = value; }
 
     private void OnEnable()
     {
@@ -34,10 +41,35 @@ public class ClientController : MonoBehaviour
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, waitForTablePosition.position, step);
         }
-        if (!waitForATable)
+        if (!waitForATable && !canInteract)
         {
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, chairPosition.position, step);
+        }
+    }
+
+    private void Update()
+    {
+        if (canInteract && timeToWaitTable > 0 && collisioning)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                canInteract = false;
+                timeToWaitTable = 0;
+                StopCoroutine("WaitingTable");
+                temporizador.text = "";
+                Table[] tables = FindObjectsOfType<Table>();
+                foreach (Table table in tables)
+                {
+                    if (table.IsEmpty)
+                    {
+                        table.IsEmpty = false;
+                        waitForATable = false;
+                        chairPosition = table.gameObject.GetComponentInChildren<Transform>();
+                        break;
+                    }
+                }
+            }
         }
     }
 
@@ -66,24 +98,26 @@ public class ClientController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (timeToWaitTable > 0 && Input.GetKeyDown(KeyCode.E) && canInteract)
-            {
-                canInteract = false;
-                timeToWaitTable = 0;
-                StopCoroutine("WaitingTable");
-                temporizador.text = "";
-                Table[] tables = FindObjectsOfType<Table>();
-                foreach (Table table in tables)
-                {
-                    if (table.IsEmpty)
-                    {
-                        table.IsEmpty = false;
-                        waitForATable = false;
-                        chairPosition = table.gameObject.GetComponentInChildren<Transform>();
-                        break;
-                    }
-                }
-            }
+            collisioning = true;
+            //if (timeToWaitTable > 0 && Input.GetKeyDown(KeyCode.E) && canInteract)
+            //{
+                
+            //    canInteract = false;
+            //    timeToWaitTable = 0;
+            //    StopCoroutine("WaitingTable");
+            //    temporizador.text = "";
+            //    Table[] tables = FindObjectsOfType<Table>();
+            //    foreach (Table table in tables)
+            //    {
+            //        if (table.IsEmpty)
+            //        {
+            //            table.IsEmpty = false;
+            //            waitForATable = false;
+            //            chairPosition = table.gameObject.GetComponentInChildren<Transform>();
+            //            break;
+            //        }
+            //    }
+            //}
         }
     }
 
@@ -92,6 +126,7 @@ public class ClientController : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             text.text = "";
+            collisioning = false;
         }
     }
 
@@ -115,4 +150,39 @@ public class ClientController : MonoBehaviour
         StopAllCoroutines();
     }
 
+    public IEnumerator WaitingToOrder()
+    {
+        timeToWaitToOrder = Random.Range(5,11);
+        while(timeToWaitToOrder > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timeToWaitToOrder--;
+        }
+        waitToOrder = Random.Range(15, 31);
+        while (waitToOrder > 0)
+        {
+            temporizador.text = waitToOrder.ToString();
+            yield return new WaitForSeconds(1f);
+            waitToOrder--;
+        }
+        if(waitToOrder <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator WaitToEat()
+    {
+        int waitFood = Random.Range(60, 81);
+        while (waitFood > 0)
+        {
+            text.text = waitFood.ToString();
+            yield return new WaitForSeconds(1f);
+            waitFood--;
+        }
+        if (waitFood <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- MonsterKitchen/Assets/Scripts/Table.cs
+++ MonsterKitchen/Assets/Scripts/Table.cs
@@ -1,15 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Table : MonoBehaviour
 {
     private bool isEmpty;
+    [SerializeField] private SOReceta[] menu;
+    private ClientController client;
+    private int flag = 0;
+    private bool triggering = false;
+    private ChefController chef;
 
     public bool IsEmpty { get => isEmpty; set => isEmpty = value; }
 
     void Start()
     {
         isEmpty = true;
+        chef = FindObjectOfType<ChefController>();
     }
+
+    private void Update()
+    {
+        if(flag == 1)
+        {
+            if(triggering && Input.GetKeyDown(KeyCode.E))
+            {
+                int index = Random.Range(0, menu.Length - 1);
+                chef.Orders.Add(menu[index]);
+                flag = 0;
+                client.Text.text = "";
+                StopCoroutine(client.WaitingToOrder());
+                StartCoroutine(client.WaitToEat());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Client"))
+        {
+            flag = 1;
+            client = other.gameObject.GetComponent<ClientController>();
+            client.WaitForOrder = true;
+            client.StartCoroutine(client.WaitingToOrder());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (flag == 1)
+        {
+            if (other.gameObject.CompareTag("Player") && client.WaitForOrder && client.WaitToOrder > 0)
+            {
+                client.Text.text = "E to interact";
+                triggering = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (flag == 1)
+        {
+            if (other.CompareTag("Player"))
+            {
+                client.Text.text = "";
+                triggering = false;
+            }
+        }
+    }
+
 }
